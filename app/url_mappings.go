db3379bcc71a0a6492824efed04dcfbea2447474
thomas hooper <tomh386@gmail.com>
2021-01-09 12:16:17 -0500

    refactor: moving controllers into separate packages

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,8 +1,13 @@
 package app
 
 import (
-	"github.com/tjhooper1/bookstore_users-api/controllers"
+	"github.com/tjhooper1/bookstore_users-api/controllers/ping"
+	"github.com/tjhooper1/bookstore_users-api/controllers/users"
 )
 func mapUrls(){
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping", ping.Ping)
+
+	router.GET("/users/:user_id", users.GetUser)
+	// router.GET("/users/search", controllers.SearchUser)
+	router.POST("/users", users.CreateUser)
 }
