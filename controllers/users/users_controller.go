2278da16a6f350691129d99c194260d81a103812
thomas hooper <tomh386@gmail.com>
2021-01-10 23:37:00 -0500

    feat: added utils

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -4,16 +4,35 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tjhooper1/bookstore_users-api/domain/users"
+	"github.com/tjhooper1/bookstore_users-api/services"
+	"github.com/tjhooper1/bookstore_users-api/utils/errors"
 )
 
-func CreateUser(c *gin.Context){
+func CreateUser(c *gin.Context) {
+	var user users.User
+
+	if err := c.BindJSON(&user); err != nil {
+		//TODO HANDLE JSON ERROR
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveError := services.CreateUser(user)
+	if saveError != nil {
+		//TODO HANDLE USER CREATION ERROR
+		c.JSON(saveError.Status, saveError)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
+}
+
+func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "implement me")
 }
 
-func GetUser(c *gin.Context){
-	c.String(http.StatusNotImplemented, "implement me")
-}
-
-func SearchUser(c *gin.Context){
+func SearchUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "implement me")
 }
